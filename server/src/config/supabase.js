42337ea467b51fd954fb45153944ffac43e16246
Juki <terijuky@gmail.com>
2026-05-19 00:02:48 +0700

    fixing #3

--- server/src/config/supabase.js
+++ server/src/config/supabase.js
@@ -1,4 +1,5 @@
-// server/src/config/supabase.js
+import dotenv from 'dotenv';
+dotenv.config();
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.SUPABASE_URL;
